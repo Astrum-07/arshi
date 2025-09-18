f720a89bf46730968710d438c95c3cb2932729c5
Astrum-07 <astrumastro07@gmail.com>
2025-09-18 07:59:37 +0500

    finally

---
+++ main.js
@@ -0,0 +1,72 @@
+
+
+// 1-misol
+// let array = [92, 45, 42, 86, 23, 56, 12, 1, 54]
+
+// let max = array[0]
+// let min = array[0]
+
+// for(let i = 1; i <= array.length; i++){
+//     if(max < array[i]){
+//         max = array[i]
+//     }
+//     console.log(array[i])
+// }
+
+// for(let i = 1; i < array.length; i++){
+//     if(min > array[i]){
+//         min = array[i]
+//     }
+// }
+
+// console.log("Max => ", max)
+// console.log("Min => ", min)
+
+
+// 2-misol
+// let reverseNumbers = (n) => {
+//     for(let i = n; i >= 1; i--){
+//         console.log(i);
+//     }
+// }
+// reverseNumbers(20);
+
+// 3-misol
+// let isPrime = (n) => {
+//     if (n <= 1) {
+//         return 'Tub  emas'; 
+//     }
+//     for(let i = 2; i < n; i++){
+//         if(n % i === 0){
+//             return 'Tub emas';
+//         }
+//     }
+//     return 'Tub son'
+// }
+
+// console.log(isPrime(1));
+
+// 4-misol
+// let reverseNumber = (n) => {
+//     for(let i = 0; i < n; i--){
+//         console.log(i);
+//     }
+// }
+// reverseNumber(123)
+
+
+// 5-misol
+// let digitSum = (n) => {
+//     let sum = 0;
+
+//     while(n > 0){
+//         let digit = n % 10;
+//         sum = sum + digit;
+//         n = Math.floor(n / 10);
+//     }
+//     return sum;
+// }
+// console.log(digitSum(999));
+
+// qolganini ishlolmadim
+
